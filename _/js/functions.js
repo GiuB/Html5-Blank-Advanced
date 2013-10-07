56008a994c7d2df789b5e7543715b7d619002a1d
GiuB HW <ginetto89@gmail.com>
2013-10-08 01:39:09 +0200

    New blank html5 tpml advanced start

---
+++ _/js/functions.js
@@ -0,0 +1,128 @@
+// Browser detection for when you get desparate. A measure of last resort.
+// http://rog.ie/post/9089341529/html5boilerplatejs
+
+// var b = document.documentElement;
+// b.setAttribute('data-useragent',  navigator.userAgent);
+// b.setAttribute('data-platform', navigator.platform);
+
+// sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
+
+//Regex email
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+//Return form non corretta
+function form_incorrect(el) {
+    console.log(el.attr('name') + " non corretto");
+    el.focus();
+    return false;
+}
+
+//Mostro le field per il login
+function show_loginFields() {
+    if (!$('.acc-logcontainer').hasClass('hover'))
+        $('.acc-logcontainer').addClass('hover');
+}
+
+//Nascondo le field per il login
+function hide_loginFields() {
+    if ($('.acc-logcontainer').hasClass('hover'))
+        $('.acc-logcontainer').removeClass('hover');
+}
+
+//Controllo del focus per il login
+function control_LoginFocus() {
+    var log_focused = false;
+    $('.acc-logcontainer input').each(function (v) {
+        if ($(this).is(':focus'))
+            log_focused = true;
+    });
+    if (log_focused) {
+        if (!$('.acc-logcontainer').hasClass('focused'))
+            $('.acc-logcontainer').addClass('focused');
+    } else {
+        if ($('.acc-logcontainer').hasClass('focused'))
+            $('.acc-logcontainer').removeClass('focused');
+    }    
+}
+
+// remap jQuery to $
+(function ($) {
+
+/* trigger when page is ready */
+$(document).ready(function () {
+    var email_validation = '^[\w\-\.]*[\w\.]\@[\w\.]*[\w\-\.]+[\w\-]+[\w]\.+[\w]+[\w $]';    
+
+    //Auto placeholder .IE
+    $('input, textarea').placeholder();
+
+    //Campi numerici
+    $(".numeric").numeric();
+    $(".integer").numeric(false, function () { alert("Solo valori interi"); this.value = ""; this.focus(); });
+    $(".positive").numeric({ negative: false }, function () { alert("Valori negativi non accettati"); this.value = ""; this.focus(); });
+    $(".positive-integer").numeric({ decimal: false, negative: false }, function () { alert("Solo valori interi positivi"); this.value = ""; this.focus(); });
+
+    //Form Design Radio / Checkbox (icheck)
+    $('input').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square',
+        increaseArea: '20%' // optional
+    });
+
+    //Controllo Generale Form
+    $('.simple_form_control .control-btn').on('click', function () {
+        var fControl = true;
+        //Controllo se tutti i campi required sono stati settati e diversi dalla precompilazione fblank per .IE
+        $.each($('.simple_form_control input, .simple_form_control textarea'), function (k, v) {
+            if (    //Validazione standard fields
+                    ($(v).attr('required') == 'required' && $(v).val().length == 0) ||
+                    //Validazione email
+                    ($(v).attr('type') == 'email' && !validateEmail($(v).val()) && $(v).attr('required') == 'required')
+                ) {
+                fControl = form_incorrect($(v));
+                return false;
+            }
+            //Validazione check privacy
+            if ($(v).hasClass('privacy_check') && !$(v).is(':checked')) {
+                fControl = form_incorrect($(v));
+                $(v).parent('.icheckbox_square-blue').addClass('hover');
+                return false;
+            }
+        });
+        if(fControl)
+            document.contact_form.submit();        
+    });
+
+    //Display evidente delle field per il login
+    $('.acc-logcontainer').hover(show_loginFields, hide_loginFields);
+    $('.acc-logcontainer input').focus(control_LoginFocus).focusout(control_LoginFocus);
+
+    //Apertura dei dettagli parent - child
+    $('.arrow-container').on('click', function () {
+        var el = $(this), child = $(this).data('child'), status = $(this).data('status');
+        if (child != null && status != null) {
+            $('#' + child).slideToggle();
+            console.log(status);
+            if (status == 'closed') { el.data('status', 'open'); el.addClass('arrow-up'); }
+            else if (status == 'open') { el.data('status', 'closed'); el.removeClass('arrow-up'); }
+        }
+    });
+
+});
+
+
+/* optional triggers
+
+$(window).load(function() {
+	
+});
+
+$(window).resize(function() {
+	
+});
+
+*/
+
+})(window.jQuery);
